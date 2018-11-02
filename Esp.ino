6c527c63c3954d12df1e9c3513f24845e207d4bd
Maxim <max@14.ru>
2018-11-02 15:01:51 +1000

    add GPS and WiFi connection

---
+++ Esp.ino
@@ -0,0 +1,145 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <WiFi.h>
+
+char ssid[] = "Robocenter615";     //  your network SSID (name)
+char pass[] = "123456789";  // your network password
+int status = WL_IDLE_STATUS; 
+static const int RXPin = D5, TXPin = D6;
+static const uint32_t GPSBaud = 9600;
+SoftwareSerial ss(RXPin, TXPin);
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ;  //  ждем подключения последовательного порта
+       // (нужно только для штатного USB-порта)
+  }
+ 
+  // задаем скорость передачи данных через порт SoftwareSerial:
+  ss.begin(GPSBaud);
+   while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+ 
+}
+Serial.print("You're connected to the network");
+  printCurrentNet();
+   printWifiData();
+}
+ 
+void loop() {
+ //printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+ //printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
+ //printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+ //printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+ //printInt(gps.location.age(), gps.location.isValid(), 5);
+   delay(10000);
+  printCurrentNet();
+}
+
+
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartDelay(0);
+}
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
+static void printInt(unsigned long val, bool valid, int len)
+{
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  smartDelay(0);
+}
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5], HEX);
+  Serial.print(":");
+  Serial.print(bssid[4], HEX);
+  Serial.print(":");
+  Serial.print(bssid[3], HEX);
+  Serial.print(":");
+  Serial.print(bssid[2], HEX);
+  Serial.print(":");
+  Serial.print(bssid[1], HEX);
+  Serial.print(":");
+  Serial.println(bssid[0], HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+}
