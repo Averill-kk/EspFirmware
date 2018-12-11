cf26f1e06326dd13cefee591cf8d3711b2fe6a1c
Maxim <max@14.ru>
2018-12-11 16:30:44 +1000

    Update Esp.ino
    bug fixes

--- Esp/Esp.ino
+++ Esp/Esp.ino
@@ -3,10 +3,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Ethernet.h>
-#include <ICMPPing.h>
+
+//Device ID
+String Id = "1";
+
 
 //WiFi Info
-const char *essid="Robocenter615";
+const char *essid="Robocenter";
 const char *key="123456789";
 
 //WebAPi Info
@@ -17,19 +20,13 @@ String ipStr = "http://10.10.143.73:5050";
 
 
 int status = WL_IDLE_STATUS; 
-static const int RXPin = D6, TXPin = D5;
+static const int RXPin = D5, TXPin = D6;
 static const uint32_t GPSBaud = 9600;
 SoftwareSerial ss(RXPin, TXPin);
 TinyGPSPlus gps;
 WiFiClient clientCommon;
-bool postRequestSend = false;
 
-IPAddress pingAddr(10,10,143,73);
-SOCKET pingSocket = 0;
-char buffer [256];
-ICMPPing ping(pingSocket, (uint16_t)random(0, 255));
 
-String Id;
 char messageId;
 void setup() {
   Serial.begin(115200);
@@ -52,30 +49,19 @@ Serial.println("WiFi connected");
 }
  
 void loop() {
-float flat, flon;
-Serial.println("Loop started");
 Serial.print("Satellites in view: ");
 Serial.println(gps.satellites.value());
-      if (gps.satellites.value()< 6)
+if (true)
       {
-        Serial.print("No fix.");
-      }
-      else
-      {
-String sendDataPost = "{\"Latitude\":"+String(gps.location.lat())+",\"Lontitude\":"+String(gps.location.lng())+",\"Satellite\":"+String(gps.satellites.value())+"}";
-if(!postRequestSend)
-{
-  //TODO Parce id from SendPostRequest string
-  Id = SendPostRequest(sendDataPost)[6];
-  postRequestSend = true;
-}else
-{
-  String sendDataPut = "{\"id\":"+Id+",\"Latitude\":"+String(gps.location.lat())+",\"Lontitude\":"+String(gps.location.lng())+",\"Satellite\":"+String(gps.satellites.value())+"}";
+//String sendDataPost = "{\"Latitude\":"+String(gps.location.lat(),11)+",\"Lontitude\":"+String(gps.location.lng(),1)+",\"Satellite\":"+String(gps.satellites.value(), DEC)+"}";
+  String sendDataPut = "{\"id\":"+Id+",\"Latitude\":"+String(gps.location.lat(),11)+",\"Lontitude\":"+String(gps.location.lng(),11)+",\"Satellite\":"+String(gps.satellites.value(), DEC)+"}";
   SendPutRequest(sendDataPut,Id);
-  
-}
-   delay(10000); 
+  delay(1000);
+  }else
+  {
+     Serial.print("No fix.");
   }
+   smartDelay(1000);
  }
 
 String SendPostRequest(String data) {
@@ -100,4 +86,14 @@ Serial.println(httpCode);   //Print HTTP return code
 Serial.println(payload);    //Print request response payload
 http.end();  //Close connection
 return payload;
+}
+
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
 }
