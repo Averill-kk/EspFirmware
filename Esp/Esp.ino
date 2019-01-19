be3fc99655a747d5f5dde21f9833039b7a4f92b8
Максим <max@14.ru>
2019-01-19 12:35:00 +1000

    add: Обновление прошивки по WiFi

--- Esp/Esp.ino
+++ Esp/Esp.ino
@@ -3,6 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Ethernet.h>
+#include <ArduinoOTA.h>
 
 //Device ID
 String id = "1";
@@ -40,6 +41,45 @@ while(WiFi.status() != WL_CONNECTED)
 }
 Serial.println("WiFi connected");
   Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  // строчка для номера порта по умолчанию
+  // можно вписать «8266»:
+  // ArduinoOTA.setPort(8266);
+ 
+  // строчка для названия хоста по умолчанию;
+  // можно вписать «esp8266-[ID чипа]»:
+  // ArduinoOTA.setHostname("myesp8266");
+ 
+  // строчка для аутентификации
+  // (по умолчанию никакой аутентификации не будет):
+  // ArduinoOTA.setPassword((const char *)"123");
+ 
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");  //  "Начало OTA-апдейта"
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");  //  "Завершение OTA-апдейта"
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    //  "Ошибка при аутентификации"
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    //  "Ошибка при начале OTA-апдейта"
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    //  "Ошибка при подключении"
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    //  "Ошибка при получении данных"
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    //  "Ошибка при завершении OTA-апдейта"
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");  //  "Готово"
+  Serial.print("IP address: ");  //  "IP-адрес: "
   Serial.println(WiFi.localIP());
 }
  
